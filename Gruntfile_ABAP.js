659b2a32484e78221d1d61328b93450d7eea3da2
sebastianmantsch@googlemail.com <haribiribreiti2>
2017-11-29 08:53:20 +0100

    [FIX] Line breaks commit test

--- Gruntfile_ABAP.js
+++ Gruntfile_ABAP.js
@@ -84,7 +84,6 @@ module.exports = function (grunt) {
         });
     };
 
-
     grunt.registerTask("createTransportRequest", "Creates an ABAP Transport Request", function () {
         grunt.log.writeln("Creating Transport Request");
         var importParameters = {
